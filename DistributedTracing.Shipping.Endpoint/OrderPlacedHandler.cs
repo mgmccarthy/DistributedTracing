3ca84cf44e5e5770fa2d983ebc154c6e1814aad1
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-12-16 11:43:00 -0500

    Add OrderPlacedHandler to Shipping.Endpoint and smoke test - shows up in Zipkin

---
+++ DistributedTracing.Shipping.Endpoint/OrderPlacedHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DistributedTracing.Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace DistributedTracing.Shipping.Endpoint
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlaced>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            Log.Info($"Handling OrderPlaced in Shipping.Endpoint with OrderId: {message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
