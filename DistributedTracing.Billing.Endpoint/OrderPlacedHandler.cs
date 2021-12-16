52e0b437283d7bc4be7b883aba0bee8196551cd4
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-12-16 11:38:26 -0500

    Add OrderPlacedHanlder to Billing.Endpoint and smoke test - shows up in Zipkin

---
+++ DistributedTracing.Billing.Endpoint/OrderPlacedHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DistributedTracing.Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace DistributedTracing.Billing.Endpoint
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlaced>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            Log.Info($"Handling OrderPlaced in Billing.Endpoint with OrderId: {message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
