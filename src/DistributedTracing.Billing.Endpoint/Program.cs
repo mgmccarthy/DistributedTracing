afe41d186e10637330946e884c495fb13f46c506
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-12-17 13:36:46 -0500

    Finally getting docker files sorted and correct

--- src/DistributedTracing.Billing.Endpoint/Program.cs
+++ src/DistributedTracing.Billing.Endpoint/Program.cs
@@ -30,9 +30,7 @@ namespace DistributedTracing.Billing.Endpoint
             ActivitySource.AddActivityListener(listener);
 
             var host = CreateHostBuilder(args).Build();
-
             InitializeSqlServer(host);
-
             host.Run();
         }
 
